95178c4c26ddc361597b58e219f8b063b66aa35d
serhatprogramming <serhatprogramming@gmail.com>
2023-03-20 10:23:14 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,82 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+const express = require("express");
+const cors = require("cors");
+const Note = require("./models/note");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static("build"));
+
+app.get("/api/notes", (req, res) => {
+  Note.find({}).then((notes) => {
+    res.json(notes);
+  });
+});
+
+app.post("/api/notes", (req, res) => {
+  const body = req.body;
+
+  if (body.content === undefined) {
+    return res.status(400).json({ error: "missing content" });
+  }
+
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+  });
+
+  note.save().then((savedNote) => {
+    res.json(savedNote);
+  });
+});
+
+app.get("/api/notes/:id", (req, res, next) => {
+  Note.findById(req.params.id)
+    .then((note) => {
+      if (note) {
+        res.json(note);
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch((e) => next(e));
+});
+
+app.delete("/api/notes/:id", (req, res, next) => {
+  Note.findByIdAndRemove(req.params.id)
+    .then((result) => {
+      res.status(204).end();
+    })
+    .catch((e) => next(e));
+});
+
+app.put("/api/notes/:id", (req, res, next) => {
+  const body = req.body;
+
+  const note = { content: body.content, important: body.important };
+
+  Note.findByIdAndUpdate(req.params.id, note, { new: true })
+    .then((updatedNote) => {
+      res.json(updatedNote);
+    })
+    .catch((e) => next(e));
+});
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  }
+
+  next(error);
+};
+
+// this has to be the last loaded middleware.
+app.use(errorHandler);
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => console.log(`The server is listening on port ${PORT}`));
